71eef68f558fc39d5767700e9aea0db8ce9ad43c
Anatolii_Kravchenko <tol890@ukr.net>
2020-12-05 17:58:32 +0300

    JS - Add Item

--- script.js
+++ script.js
@@ -101,6 +101,31 @@ function updateDOM() {
 
 }
 
+// Add To Column List
+function addToColumn(column) {
+    const itemText = addItems[column].textContent;
+    const selectedarray = listArrays[column];
+    selectedarray.push(itemText);
+    addItems[column].textContent = '';
+    updateDOM();
+
+}
+
+// Show Add Item Input Box
+function showInputBox(column) {
+    addBtns[column].style.visibility = 'hidden';
+    saveItemBtns[column].style.display = 'flex';
+    addItemContainers[column].style.display = 'flex';
+}
+
+// Hide Input Box
+function hideInputBox(column) {
+    addBtns[column].style.visibility = 'visible';
+    saveItemBtns[column].style.display = 'none';
+    addItemContainers[column].style.display = 'none';
+    addToColumn(column);
+}
+
 // Allows arrays to reflect Drag and Drop items
 function rebuildArrays() {
     backlogListArray = [];
